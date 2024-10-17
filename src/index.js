58a5d5c801ea07e9d8a92654c845fed2e86c1284
JosMiguelMM2 <josemmendez@ucundinamarca.edu.co>
2024-10-16 22:35:29 -0500

    html

--- src/index.js
+++ src/index.js
@@ -2,48 +2,46 @@ const express = require('express');
 const ldap = require('ldapjs');
 const bodyParser = require('body-parser');
 const cors = require('cors'); // Importar cors
-
 const app = express();
+
+// configuracion de cors
 app.use(cors({
-    origin:    'ldap://infosecure.com',  //'http://exptech.local', // Permitir solicitudes desde este origen
-    methods: 'GET,POST', // Métodos permitidos
-    allowedHeaders: 'Content-Type', // Encabezados permitidos
+    origin:    'ldap://infosecure.com',  
+    methods: 'GET,POST', 
+    allowedHeaders: 'Content-Type', 
 }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Configuración del servidor LDAP (modificar según tu dominio y servidor AD)
+// configuracion con el dominio de la maquina con el puerto configurado
 const LDAP_URL = 'ldap://infosecure.com';
-//const LDAP_BASE_DN = 'dc=infosecure,dc=com';
 
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
-    // Validación simple de entrada
     if (!username || !password) {
         return res.status(400).send('Usuario y contraseña son requeridos.');
     }
 
-    // Crear un cliente LDAP
+
     const client = ldap.createClient({
         url: LDAP_URL
     });
     console.log(`Usuario: ${username}, Contraseña: ${password}`);
-    const userDN = `INFOSECURE0\\${username}`; // Usar la barra invertida doble para escapar
+    const userDN = `INFOSECURE0\\${username}`; 
 
     console.log("usuario ", userDN)
-    // Intentar autenticar al usuario
+
+    // se autentica el usuario
     client.bind(userDN, password, (err) => {
         if (err) {
             console.error('Error en la autenticación:', err);
-            return res.status(401).send({mensage:'Credenciales inválidas.'}); // Mensaje genérico
+            return res.status(403).send({mensage:'Credenciales inválidas.'});
         }
 
-        // Si la autenticación es exitosa
         res.status(200).send({mensaje:'Autenticación exitosa'});
         
-        // Desconectar del cliente LDAP después de la autenticación
         client.unbind((unbindErr) => {
             if (unbindErr) {
                 console.error('Error al desconectar del cliente LDAP:', unbindErr);
